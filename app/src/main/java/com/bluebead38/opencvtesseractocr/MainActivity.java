de9a4994eb484a8636b116b09e8507d06d735b60
skdlaslek <43161054+skdlaslek@users.noreply.github.com>
2018-11-05 20:42:39 +0900

    code

---
+++ app/src/main/java/com/bluebead38/opencvtesseractocr/MainActivity.java
@@ -0,0 +1,194 @@
+package com.bluebead38.opencvtesseractocr;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.provider.ContactsContract;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnCameraView;
+    private Button mBtnPhoneContacts;
+    private EditText mEditOcrResult;
+    private TextView displayEmail;
+    private TextView displayPhone;
+    private TextView displayName;
+    private String datapath = "";
+    private String lang = "";
+
+    private int ACTIVITY_REQUEST_CODE = 1;
+
+    static TessBaseAPI sTess;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // 뷰 선언
+        mBtnCameraView = (Button) findViewById(R.id.btn_camera);
+        mBtnPhoneContacts = (Button) findViewById(R.id.btn_to_phone);
+        mEditOcrResult = (EditText) findViewById(R.id.edit_ocrresult);
+        displayName = (TextView) findViewById(R.id.text_name);
+        displayPhone = (TextView) findViewById(R.id.text_phone);
+        displayEmail = (TextView) findViewById(R.id.text_email);
+        sTess = new TessBaseAPI();
+
+
+        // Tesseract 인식 언어를 한국어로 설정 및 초기화
+        lang = "eng";
+        datapath = getFilesDir()+ "/tesseract";
+
+        if(checkFile(new File(datapath+"/tessdata")))
+        {
+            sTess.init(datapath, lang);
+        }
+
+        mBtnCameraView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent mIttCamera = new Intent(MainActivity.this, com.bluebead38.opencvtesseractocr.CameraView.class);
+                startActivityForResult(mIttCamera, ACTIVITY_REQUEST_CODE);
+            }
+        });
+
+        mBtnPhoneContacts.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+                intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+
+                intent.putExtra(ContactsContract.Intents.Insert.NAME, displayName.getText());
+
+                intent.putExtra(ContactsContract.Intents.Insert.EMAIL, displayEmail.getText());
+                intent .putExtra(ContactsContract.Intents.Insert.EMAIL_TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK);
+
+                intent.putExtra(ContactsContract.Intents.Insert.PHONE, displayPhone.getText());
+                intent.putExtra(ContactsContract.Intents.Insert.PHONE_TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_WORK);
+
+                startActivity(intent);
+                //finish();
+            }
+        });
+    }
+
+    boolean checkFile(File dir)
+    {
+        //디렉토리가 없으면 디렉토리를 만들고 그 후에 파일을 카피
+        if(!dir.exists() && dir.mkdirs()) {
+            copyFiles();
+        }
+        //디렉토리가 있지만 파일이 없으면 파일카피 진행
+        if(dir.exists()) {
+            String datafilepath = datapath + "/tessdata/" + lang + ".traineddata";
+            File datafile = new File(datafilepath);
+            if(!datafile.exists()) {
+                copyFiles();
+            }
+        }
+        return true;
+    }
+
+    void copyFiles()
+    {
+        AssetManager assetMgr = this.getAssets();
+
+        InputStream is = null;
+        OutputStream os = null;
+
+        try {
+            is = assetMgr.open("tessdata/"+lang+".traineddata");
+
+            String destFile = datapath + "/tessdata/" + lang + ".traineddata";
+
+            os = new FileOutputStream(destFile);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = is.read(buffer)) != -1) {
+                os.write(buffer, 0, read);
+            }
+            is.close();
+            os.flush();
+            os.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void extractName(String str){
+        System.out.println("Getting the Name");
+        final String NAME_REGEX = "^([A-Z]([a-z]*|\\.) *){1,2}([A-Z][a-z]+-?)+$";
+        Pattern p = Pattern.compile(NAME_REGEX, Pattern.MULTILINE);
+        Matcher m =  p.matcher(str);
+        if(m.find()){
+            System.out.println(m.group());
+            displayName.setText(m.group());
+        }
+    }
+
+    public void extractEmail(String str) {
+        System.out.println("Getting the email");
+        final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+        Pattern p = Pattern.compile(EMAIL_REGEX, Pattern.MULTILINE);
+        Matcher m = p.matcher(str);   // get a matcher object
+        if(m.find()){
+            System.out.println(m.group());
+            displayEmail.setText(m.group());
+        }
+    }
+
+    public void extractPhone(String str){
+        System.out.println("Getting Phone Number");
+        final String PHONE_REGEX="(?:^|\\D)(\\d{3})[)\\-. ]*?(\\d{3})[\\-. ]*?(\\d{4})(?:$|\\D)";
+        Pattern p = Pattern.compile(PHONE_REGEX, Pattern.MULTILINE);
+        Matcher m = p.matcher(str);   // get a matcher object
+        if(m.find()){
+            System.out.println(m.group());
+            displayPhone.setText(m.group());
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String result = "";
+        if(resultCode==RESULT_OK)
+        {
+            if(requestCode== ACTIVITY_REQUEST_CODE)
+            {
+                // 받아온 OCR 결과 출력
+                result = data.getStringExtra("STRING_OCR_RESULT");
+                mEditOcrResult.setText(result);
+                extractName(result);
+                extractPhone(result);
+                extractEmail(result);
+            }
+        }
+    }
+}
