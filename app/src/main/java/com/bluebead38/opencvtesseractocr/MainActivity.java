3d11a76ae80df8b4455fca28770fce28d606e1cd
skdlaslek <43161054+skdlaslek@users.noreply.github.com>
2018-12-22 13:47:34 +0900

    Update MainActivity.java

--- app/src/main/java/com/bluebead38/opencvtesseractocr/MainActivity.java
+++ app/src/main/java/com/bluebead38/opencvtesseractocr/MainActivity.java
@@ -30,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView displayEmail;
     private TextView displayPhone;
     private TextView displayName;
+    private TextView displayMPhone;
+    private TextView displayWeb;
     private String datapath = "";
     private String lang = "";
 
@@ -48,13 +50,15 @@ public class MainActivity extends AppCompatActivity {
         mBtnPhoneContacts = (Button) findViewById(R.id.btn_to_phone);
         mEditOcrResult = (EditText) findViewById(R.id.edit_ocrresult);
         displayName = (TextView) findViewById(R.id.text_name);
-        displayPhone = (TextView) findViewById(R.id.text_Mphone);
+        displayPhone = (TextView) findViewById(R.id.text_Hphone);
         displayEmail = (TextView) findViewById(R.id.text_email);
+        displayMPhone = (TextView) findViewById(R.id.text_Mphone);
+        displayWeb = (TextView) findViewById(R.id.text_web);
         sTess = new TessBaseAPI();
 
 
         // Tesseract 인식 언어를 한국어로 설정 및 초기화
-        lang = "eng";
+        lang = "kor";
         datapath = getFilesDir()+ "/tesseract";
 
         if(checkFile(new File(datapath+"/tessdata")))
@@ -141,28 +145,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void extractName(String str){
-        final String NAME_REGEX = "^([A-Z]([a-z]*|\\.) *){1,2}([A-Z][a-z]+-?)+$";
+        final String NAME_REGEX = "(김|이|박|최|정|강|조|윤|장|임|한|오|서|신|권|황|안|송|전|홍|류|고|문|량|양|손|배|백|허|남|심|로|노|하|곽|성|차|주|우|구|민|유|나|진|지|엄|채|원|천|방|공|현|함|변|염|여|추|도|소|석|선|설|마|길|연|위|표|명|기|반|라|왕|금|인|옥|육|맹|제|모|남궁|탁|국|어|은|편|용|예|경|봉|사|부|황보|가|복|태|목|형|계|피|두|감|제갈|음|빈|동|온|사공|호|범|선우|좌|팽)+((\\s)?[가-힣]){1,3}(\\s)?$";
         Pattern p = Pattern.compile(NAME_REGEX, Pattern.MULTILINE);
         Matcher m =  p.matcher(str);
         if(m.find()){
+            System.out.println(m.group());
             displayName.setText(m.group());
         }
     }
 
     public void extractEmail(String str) {
-        final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+        final String EMAIL_REGEX = "([a-z0-9_\\.-]+)@([\\da-zA-Z\\.-]+)\\.([a-zA-z\\.]{2,6})";
         Pattern p = Pattern.compile(EMAIL_REGEX, Pattern.MULTILINE);
         Matcher m = p.matcher(str);   // get a matcher object
         if(m.find()){
+            System.out.println(m.group());
             displayEmail.setText(m.group());
         }
     }
 
-    public void extractPhone(String str){
-        final String PHONE_REGEX="(?:^|\\D)(\\d{3})[)\\-. ]*?(\\d{3})[\\-. ]*?(\\d{4})(?:$|\\D)";
-        Pattern p = Pattern.compile(PHONE_REGEX, Pattern.MULTILINE);
+    public void extractWebPage(String str) {
+        System.out.println("Getting the webpage");
+        final String EMAIL_REGEX = "[w|W]{3}\\.([\\da-zA-Z\\.-_]+)\\.([a-zA-Z\\.]{2,6})";
+        Pattern p = Pattern.compile(EMAIL_REGEX, Pattern.MULTILINE);
         Matcher m = p.matcher(str);   // get a matcher object
         if(m.find()){
+            System.out.println(m.group());
+            displayWeb.setText(m.group());
+        }
+    }
+
+    public void extractHomeNum(String str) {
+        System.out.println("Getting Home Number");
+        final String HPHONE_REGEX = "0[2-9][0-9]?[)\\-. ]*(\\d{3,4})[\\-. ]*(\\d{4})";
+        Pattern p = Pattern.compile(HPHONE_REGEX, Pattern.MULTILINE);
+        Matcher m = p.matcher(str);   // get a matcher object
+        if (m.find()) {
+            System.out.println(m.group());
+            displayMPhone.setText(m.group());
+        }
+    }
+
+    public void extractMobileNum(String str) {
+        System.out.println("Getting Mobile Number");
+        final String MPHONE_REGEX = "01[0|1|6|7|8|9][)\\-. ]*(\\d{3,4})[\\-. ]*(\\d{4})";
+        Pattern p = Pattern.compile(MPHONE_REGEX, Pattern.MULTILINE);
+        Matcher m = p.matcher(str);   // get a matcher object
+        if (m.find()) {
+            System.out.println(m.group());
             displayPhone.setText(m.group());
         }
     }
@@ -176,10 +206,23 @@ public class MainActivity extends AppCompatActivity {
             {
                 // 받아온 OCR 결과 출력
                 result = data.getStringExtra("STRING_OCR_RESULT");
+
+                ///////////////////////////////////////////////////////////////////수정한 부분에서 에러나는 것 없에면 잘 실행됨.
+                /*result= result.replace("|", "l");
+                result= result.replace("ⓒ", "@");   //일단 ⓒ만 수정
+                String mail1="com";      //기본 com으로 설정. 추후에 추가
+                StringBuffer sb = new StringBuffer(result);
+                int idex = result.indexOf(mail1);
+                if(result.charAt(idex-1)!='.')
+                    sb.insert('.',idex);*/
+
                 mEditOcrResult.setText(result);
+
                 extractName(result);
-                extractPhone(result);
+                extractMobileNum(result);
+                extractHomeNum(result);
                 extractEmail(result);
+                extractWebPage(result);
             }
         }
     }
